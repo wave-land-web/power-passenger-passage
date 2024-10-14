f2a2eaf443e3af41fa4e145da8f7d023c5387c5d
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-10-14 15:16:22 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://powerpassengerpassage.com',
+  output: 'hybrid',
+  prefetch: {
+    prefetchAll: true,
+  },
+  integrations: [
+    tailwind(),
+    icon(),
+    sitemap({
+      lastmod: new Date(),
+    }),
+  ],
+  adapter: netlify({
+    imageCDN: false,
+    cacheOnDemandPages: true,
+  }),
+})
