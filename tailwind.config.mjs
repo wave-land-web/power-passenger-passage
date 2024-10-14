5ee8c369aad6113fbacbde64932e813583e525c7
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-10-14 17:57:46 -0500

    feat(app): basic layout and global styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      white: 'var(--color-white)',
+      black: 'var(--color-black)',
+      grey: 'var(--color-grey)',
+      slate: 'var(--color-slate)',
+      red: {
+        900: 'var(--color-red-900)',
+        500: 'var(--color-red-500)',
+        100: 'var(--color-red-100)',
+      },
+      tan: {
+        100: 'var(--color-tan-100)',
+      },
+    },
+    extend: {
+      fontFamily: {
+        header: ['Alegreya', 'serif'],
+        body: ['Alegreya Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
 }
