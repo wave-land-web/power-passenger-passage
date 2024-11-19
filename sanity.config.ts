34fcfda44a97fc7e8871f38868dbb34ddff38ed3
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-11-19 14:12:06 -0600

    feat(app): add correct date and read time to posts, vision tools for dev

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'sanity'
+import { visionTool } from '@sanity/vision'
+import { defineConfig, isDev } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { schema } from './src/sanity/schemaTypes'
+
+const devOnlyPlugins = [structureTool(), visionTool()]
 
 export default defineConfig({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-  plugins: [structureTool()],
+  plugins: isDev ? devOnlyPlugins : [structureTool()],
   schema,
 })
