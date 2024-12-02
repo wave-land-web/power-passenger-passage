668ead624f5cddf14e1968e158eabf65dc9ba87b
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-12-02 11:25:28 -0600

    test

--- src/pages/api/create-email.json.ts
+++ src/pages/api/create-email.json.ts
@@ -1,24 +1,22 @@
 export const prerender = false
 
-import type { SanityDocument } from '@sanity/client'
 import type { APIRoute } from 'astro'
-import { resend } from '../../../lib/resend'
-import { loadQuery } from '../../../sanity/lib/load-query'
+import { resend } from '../../lib/resend'
 
 export const GET: APIRoute = async ({ params, request }) => {
   console.log('params', params)
   console.log('request', request)
-  const { id } = params
+  console.log('request body: ', request.body)
 
   // Get post with given pararm id from Sanity
-  const { data: post } = await loadQuery<SanityDocument>({
-    query: `*[_type == "post" && _id == ${id}]`,
-  })
+  // const { data: post } = await loadQuery<SanityDocument>({
+  //   query: `*[_type == "post" && _id == ${id}]`,
+  // })
 
   const { data, error } = await resend.emails.send({
     from: 'Power Passenger Passage <josh@wavelandweb.com>',
     to: 'josh@wavelandweb.com',
-    subject: `New Post: ${post.title}`,
+    subject: `New Post: `,
     html: /* HTML */ `<body
       style='font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"; color: #0E1726; margin-bottom: 36px;'
     >
@@ -30,15 +28,13 @@ export const GET: APIRoute = async ({ params, request }) => {
         width="170"
       />
 
-      <h1 style="font-size: 24px; font-weight: 700; margin-bottom: 12px;">
-        New Post: ${post.title}
-      </h1>
+      <h1 style="font-size: 24px; font-weight: 700; margin-bottom: 12px;">New Post:</h1>
     </body>`,
   })
 
-  return new Response(JSON.stringify(post), {
-    headers: {
-      'content-type': 'application/json',
-    },
-  })
+  return new Response(
+    JSON.stringify({
+      body: request.body,
+    })
+  )
 }
