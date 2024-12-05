941a050c431c82a6c69f8f80a554151dde4a1f91
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-12-05 15:20:03 -0600

    feat(app): add descriptions and icon to Sanity Newsletter doc

--- src/sanity/schemaTypes/newsletter.ts
+++ src/sanity/schemaTypes/newsletter.ts
@@ -1,26 +1,29 @@
-import { DocumentIcon } from '@sanity/icons'
+import { EnvelopeIcon } from '@sanity/icons'
 import { defineField, defineType } from 'sanity'
 
 export default defineType({
   name: 'newsletter',
   title: 'Newsletter',
   type: 'document',
-  icon: DocumentIcon,
+  icon: EnvelopeIcon,
   fields: [
     defineField({
       name: 'subject',
       title: 'Subject',
+      description: 'Please write an attention-grabbing headline',
       type: 'string',
     }),
     defineField({
       name: 'preview',
       title: 'Preview',
+      description:
+        'Please provide a short preview of your email - ex/ "Enjoy a super sweet discount, on us!"',
       type: 'string',
     }),
-
     defineField({
       name: 'body',
       title: 'Body',
+      description: 'Add email content here',
       type: 'blockContent',
     }),
   ],
